7b98c9b16dcf5e70219b9d9cb6b0bd7800167542
joao <joaosb004@gmail.com>
2023-05-11 15:35:13 -0300

    feat/PUT customer

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -39,9 +39,36 @@ export async function getCustomersById(req, res) {
 
     try {
 
-        const games = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id]);
-        if(games.rows.length <= 0) return res.sendStatus(404);
-        res.send(games.rows);
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id]);
+        if (customer.rows.length <= 0) return res.sendStatus(404);
+        res.send(customer.rows);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
+}
+
+export async function updateCustomer(req, res) {
+
+    const { id } = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id]);
+        const customer1 = await db.query(`SELECT * FROM customers WHERE cpf=$1;`, [cpf]);
+        if (customer.rows.length <= 0) return res.sendStatus(404);
+
+        if (customer.rows[0].cpf === cpf || customer1.rows[0]?.id == null || customer1.rows[0]?.id === customer.rows[0].id) {
+            await db.query(`UPDATE customers
+            SET "name" = $1, "phone" = $2, cpf = $3, birthday = $4
+            WHERE id = $5;`, [name, phone, cpf, birthday, id]);
+        } else {
+            return res.sendStatus(409);
+        }
+
+        res.send();
 
     } catch (err) {
         res.status(500).send(err.message);
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
-import { getCustomers, getCustomersById, postCostumer } from "../controllers/customers.controller.js";
+import { getCustomers, getCustomersById, postCostumer, updateCustomer } from "../controllers/customers.controller.js";
 import customerSchema from "../schemas/costumers.schema.js";
 
 const customersRouter = Router();
@@ -8,5 +8,6 @@ const customersRouter = Router();
 customersRouter.post("/customers", validateSchema(customerSchema), postCostumer);
 customersRouter.get("/customers", getCustomers);
 customersRouter.get("/customers/:id", getCustomersById);
+customersRouter.put("/customers/:id", validateSchema(customerSchema), updateCustomer);
 
 export default customersRouter;

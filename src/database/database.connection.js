5dd26408fb789ae9a770151a61216b61e9d3ca34
joao <joaosb004@gmail.com>
2023-05-15 17:13:28 -0300

    Deploy adjust

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -9,5 +9,7 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
 const db = new Pool(configDatabase);
 export default db;
